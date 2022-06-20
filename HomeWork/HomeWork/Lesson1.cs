b3d46205202bd9f12f70832d248ad7de24f9e225
Elizaveta <terabitia95@gmail.com>
2022-06-21 00:23:06 +0300

    Lesson1

---
+++ HomeWork/HomeWork/Lesson1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Lesson1
+    {
+        static void Main(string[] args)
+        {
+            byte age;
+            short health;
+            int phoneNumber;
+            uint years;
+            long money;
+            float damage;
+            double coordinate;
+            char smail;
+            string name;
+            bool isDead;
+        }
+    }
+}
