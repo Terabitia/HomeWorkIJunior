c05cb6c7fd81aa51b3081a226f74da6e7332535b
Terabitia <107889129+Terabitia@users.noreply.github.com>
2022-09-18 20:54:55 +0300

    fix

--- HomeWork/HomeWork/Lesson11.cs
+++ HomeWork/HomeWork/Lesson11.cs
@@ -11,23 +11,25 @@ namespace HomeWork
         static void Main(string[] args)
         {
             Random random = new Random();
+
             int minNamber = 0;
             int maxNamber = 101;
             int namber = random.Next(minNamber, maxNamber);
             int sum = 0;
             int positiveNumber;
-            int multipleThree = 3;
-            int multipleFive = 5; 
+            int multipleMin = 3;
+            int multipleMax = 5; 
+
             Console.WriteLine("Случайное число: "+ namber);
 
             for (positiveNumber = 0; positiveNumber <= namber; positiveNumber++)
             {
-                if ((positiveNumber % multipleThree == 0) || (positiveNumber % multipleFive == 0))
+                if ((positiveNumber % multipleMin == 0) || (positiveNumber % multipleMax == 0))
                 {
                     sum += positiveNumber;
                 }
             }
-            Console.WriteLine("Сумма всех чисел кратные 3 или 5: " + sum);
+            Console.WriteLine("Сумма всех чисел кратные "+ multipleMin +" или " + multipleMax+": " + sum);
         }
     }
 }
